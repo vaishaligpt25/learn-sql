a42fa00aab36bfefb8a91d6a726ff467ff28f412
vaishaligpt25 <vaishali.gpt25@gmail.com>
2025-02-19 11:38:02 +0000

    lesson_12_select_queries_order_of_execution

--- sqlbolt/lesson_12_select_queries_order_of_execution.sql
+++ sqlbolt/lesson_12_select_queries_order_of_execution.sql
@@ -1,8 +1,21 @@
 -- https://sqlbolt.com/lesson/select_queries_order_of_execution
 
+-- 1.................................................
+
 SELECT
     Director, COUNT(Title)
 FROM
     Movies
 GROUP BY
     Director;
+
+-- 2..............................................................
+
+SELECT
+    Director,
+    SUM(Domestic_sales + International_sales) AS Cumulative_Sales_from_all_movies
+FROM Movies
+ JOIN Boxoffice
+   ON Movies.ID = Boxoffice.Movie_ID
+GROUP BY Director;
+
