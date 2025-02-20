e9bb214cb2cacdf03bb550f07b1d9c576da76a4f
vaishaligpt25 <vaishali.gpt25@gmail.com>
2025-02-20 11:19:49 +0000

    (hacker-rank/weather-observation-station-18)

---
+++ hacker-rank /weather-observation-station-18.sql
@@ -0,0 +1,8 @@
+-- https://www.hackerrank.com/challenges/weather-observation-station-18
+
+SELECT ROUND(
+    ABS(MIN(LAT_N) - MAX(LAT_N)) +
+    ABS(MIN(LONG_W) - MAX(LONG_W)),
+    4)
+FROM STATION;
+
